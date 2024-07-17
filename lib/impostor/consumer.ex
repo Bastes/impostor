f2e1f98ea5434bc33ee57652f033693786b695a6
Michel Belleville <michel.belleville@gmail.com>
2024-07-17 20:53:12 +0200

    new game... that does not actually create a new game, in fact ; sorry

--- lib/impostor/consumer.ex
+++ lib/impostor/consumer.ex
@@ -1,10 +1,30 @@
 defmodule Impostor.Consumer do
   use Nostrum.Consumer
 
+  alias Nostrum.Api
+  alias Nostrum.Struct.Embed
+
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+    IO.inspect(msg, label: "msg")
+
     case msg.content do
-      "!word" ->
-        Nostrum.Api.create_message(msg.channel_id, "Word, brother!")
+      "!new_impostor_game" ->
+        embed =
+          %Embed{}
+          |> Embed.put_title("New Game of Impostor")
+          |> Embed.put_description(
+            """
+            Let's prepare your new game.
+
+            Players:
+            #{msg.author.global_name} (@#{msg.author.username})
+            """
+          )
+
+        Api.create_message(
+          msg.channel_id,
+          embeds: [embed]
+        )
     end
   end
 end
